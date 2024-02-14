21bc5d7d0659a76a64d8790c6da91071f9694fc2
Adarsh <patat1234666@gmail.com>
2024-02-14 10:22:41 +0530

    Add Py AGain

--- bi_subtask/models/task.py
+++ bi_subtask/models/task.py
@@ -35,28 +35,29 @@ class subtask_wizard(models.Model):
 
     subtask_lines = fields.One2many('project.task', 'wiz_id', string="Task Line")
 
-    # def create_subtask(self):
-    #     list_of_stage = []
-    #     project_task_id = self.env['project.task'].browse(self._context.get('active_id'))
-    #     for stage in project_task_id.project_id.type_ids:
-    #         stage_ids = self.env['project.task.type'].search([('id', '=', stage.id)])
-    #         list_of_stage.append(stage_ids.id)
-    #     for task in self.subtask_lines:
-    #         task.task_parent_id = self._context.get('active_id')
-    #         task.description = task.des
-    #         task.is_subtask = True
-    #         task.project_id = project_task_id.project_id.id
-    #         if list_of_stage:
-    #             task.stage_id = list_of_stage[0]
-    #
-    #     return True
+    def create_subtask(self):
+        list_of_stage = []
+        project_task_id = self.env['project.task'].browse(self._context.get('active_id'))
+        for stage in project_task_id.project_id.type_ids:
+            stage_ids = self.env['project.task.type'].search([('id', '=', stage.id)])
+            list_of_stage.append(stage_ids.id)
+        for task in self.subtask_lines:
+            task.task_parent_id = self._context.get('active_id')
+            task.description = task.des
+            task.is_subtask = True
+            task.project_id = project_task_id.project_id.id
+            if list_of_stage:
+                task.stage_id = list_of_stage[0]
+
+        return True
 
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
     wiz_id = fields.Many2one('subtask.wizard', string="Wiz Parent Id")
-    # task_parent_id = fields.Many2one('project.task', string="Parent Id" )
+    task_parent_id = fields.Many2one('project.task', string="Parent Id" )
+    subtask_ids = fields.One2many('project.task', 'task_parent_id', string="Subtask")
     test = fields.Char("Test")
     des = fields.Char('Task Description')
     is_subtask = fields.Boolean('Is a subtask')
@@ -81,48 +82,48 @@ class ProjectTask(models.Model):
 #             error_message = _('You cannot write on %s fields in task.', ', '.join(unauthorized_fields))
 #
 #
-#     @api.onchange('parent_id')
-#     def button_disable(self):
-#         if self.parent_id:
-#             self.is_subtask = True
-#             self.task_parent_id = self.parent_id.id
-#             self.project_id = self.parent_id.project_id
-#         else:
-#             self.is_subtask = False
-#             self.project_id = False
-#
-#     def cancel_test(self):
-#         return {
-#             'type': 'ir.actions.client',
-#             'tag': 'reload',
-#         }
-#
-#     def _compute_subtask_count(self):
-#         result = super(ProjectTask, self)._compute_subtask_count()
-#         for task in self:
-#             task_ids = self.env['project.task'].search([('task_parent_id', '=', task.id)])
-#             if task_ids:
-#                 task.subtask_count = len(task_ids)
-#                 task.parent_id = task.task_parent_id
-#         return result
-#
-#     @api.onchange('stage_id')
-#     def button_is_visible(self):
-#         if self.stage_id.name == 'Done' or self.stage_id.name == 'done' and self.parent_id and not self.task_parent_id:
-#             self.is_subtask = True
-#         else:
-#             self.is_subtask = False
-#
-#
-#
-#     def write(self, vals):
-#         if vals.get('stage_id'):
-#             task_type_search = self.env['ir.config_parameter'].sudo().get_param('bi_subtask.warning_child_task')
-#
-#             if task_type_search:
-#
-#                 if vals.get('stage_id') == int(task_type_search):
-#                     for task in self.subtask_ids:
-#                         if task.stage_id.id != int(task_type_search):
-#                             raise UserError("You can not close parent task until all child tasks are closed.")
-#         return super(ProjectTask, self).write(vals)
+    @api.onchange('parent_id')
+    def button_disable(self):
+        if self.parent_id:
+            self.is_subtask = True
+            self.task_parent_id = self.parent_id.id
+            self.project_id = self.parent_id.project_id
+        else:
+            self.is_subtask = False
+            self.project_id = False
+
+    def cancel_test(self):
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+
+    def _compute_subtask_count(self):
+        result = super(ProjectTask, self)._compute_subtask_count()
+        for task in self:
+            task_ids = self.env['project.task'].search([('task_parent_id', '=', task.id)])
+            if task_ids:
+                task.subtask_count = len(task_ids)
+                task.parent_id = task.task_parent_id
+        return result
+
+    @api.onchange('stage_id')
+    def button_is_visible(self):
+        if self.stage_id.name == 'Done' or self.stage_id.name == 'done' and self.parent_id and not self.task_parent_id:
+            self.is_subtask = True
+        else:
+            self.is_subtask = False
+
+
+
+    def write(self, vals):
+        if vals.get('stage_id'):
+            task_type_search = self.env['ir.config_parameter'].sudo().get_param('bi_subtask.warning_child_task')
+
+            if task_type_search:
+
+                if vals.get('stage_id') == int(task_type_search):
+                    for task in self.subtask_ids:
+                        if task.stage_id.id != int(task_type_search):
+                            raise UserError("You can not close parent task until all child tasks are closed.")
+        return super(ProjectTask, self).write(vals)
